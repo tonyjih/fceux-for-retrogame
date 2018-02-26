8f73aba6a778bff80ac91223226e37a578112cf8
Tony Jih <tonyjih@gmail.com>
2018-02-27 00:12:03 +0800

    試作型擬似逐行掃描(?)

--- src/drivers/dingux-sdl/dingoo-video.cpp
+++ src/drivers/dingux-sdl/dingoo-video.cpp
@@ -454,9 +454,13 @@ void BlitScreen(uint8 *XBuf) {
 		for (y = s_tlines; y; y--, src += 256 - NWIDTH) {
 			for (x = NWIDTH >> 3; x; x--) {
 				__builtin_prefetch(dest + 4, 1);
+				*(dest + 160) = palettetranslate[*(uint16 *) src];
 				*dest++ = palettetranslate[*(uint16 *) src];
+				*(dest + 160) = palettetranslate[*(uint16 *) (src + 2)];
 				*dest++ = palettetranslate[*(uint16 *) (src + 2)];
+				*(dest + 160) = palettetranslate[*(uint16 *) (src + 4)];
 				*dest++ = palettetranslate[*(uint16 *) (src + 4)];
+				*(dest + 160) = palettetranslate[*(uint16 *) (src + 6)];
 				*dest++ = palettetranslate[*(uint16 *) (src + 6)];
 				src += 8;
 			}
